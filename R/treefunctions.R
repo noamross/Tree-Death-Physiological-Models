dc29633859eccb43c5227bc001570615cf13e71e
noamross <noam.ross@gmail.com>
2011-02-17 13:34:08 -0800

    First run of tree model

--- R/treefunctions.R
+++ R/treefunctions.R
@@ -2,31 +2,32 @@
 #Noam Ross Started February 15, 2011
 
 #Set run variables
-times = #times at which to solve the model
+times = 1:120 #times at which to solve the model
 states0 = c( #a vector of initial state variables
-  W = , #soil water potential, MPa
-  D = , #leaf surface moisture saturation deficit, MPa
-  S = , #total non-structural carbohydrates, g
-  K = , #xylem conductance, mmol s-1 MPa-1 
+  W = -1, #soil water potential, MPa
+  D = -2, #leaf surface moisture saturation deficit, MPa
+  S = 5, #total non-structural carbohydrates, g
+  K = 1 #xylem conductance, mmol s-1 MPa-1 
 )
-  soil water potential )
+
 
 parms = list(
-  alpha = , #photsynthetic rate per stomatal conductane, in g s MPa mmol-1 d-1
-  beta = , #repair coefficient in g g-1 d-1
-  gamma = , #growth coefficient in g g-1 d-1
-  theta = , #repair efficacy, in mmol s-1 MPa-1 d-1 g-1
-  kmax = , #maximum hydraulic contuctance, in mmol s-1 MPa-1
-  m = , #minimum maintenance respiration carb allocation, in g d-1
-  l.B = , #scale parameter for growth weibull function
-  k.B = , #shape parameter for growth weibull function
-  l.K = , #scale parameter for conductance weibull function
-  k.K = , #shape paramter for conductance weibull function
+  alpha = 1, #photsynthetic rate per stomatal conductane, in g s MPa mmol-1 d-1
+  beta = 1, #repair coefficient in g g-1 d-1
+  gamma = 1, #growth coefficient in g g-1 d-1
+  theta = 1, #repair efficacy, in mmol s-1 MPa-1 d-1 g-1
+  kmax = 10, #maximum hydraulic contuctance, in mmol s-1 MPa-1
+  m = 0, #minimum maintenance respiration carb allocation, in g d-1
+  l.B = 1, #scale parameter for growth weibull function
+  k.B = 1, #shape parameter for growth weibull function
+  l.K = 1, #scale parameter for conductance weibull function
+  k.K = 1, #shape paramter for conductance weibull function
   #Gmin = 0 , #minimum stomatal conductance, mmol s-1 MPa-1
   gmax = 1,  #maximum stomatal conductance, mmol s-1 MPa-1
   #l.G = 1 , #scale parameter for stomatal closing weibull function
   #k.G = 5 , #shape parameter for stomatal closing weibull function
-  gs = ,#stomatal closure factor
+  gs = 1, #stomatal closure factor
+  wpg = 0.5 #water potential of gravity, MPa
 )
   
 
@@ -37,16 +38,16 @@ tree_odes = function(time, states, parms) {
 # states: a vector of state variables
 # parms:  a list of parameters for the model
   
-  with(as.list(parms), { #extract parameters from 'parms' vector
+  with(c(as.list(states), parms), { #extract parameters from 'parms' vector
     X = (W - D*gmax/K - wpg)/(1 + D/(K*gs)) # Calculate xylem pressure
-    G = gmax - (-X/gs) # Calculate stomatal flow.  These are at
+    G = gmax - (-X/gs) # Calculate stomatal flow.  
     
     dW = 0  #no change in soil water potential or 
     dD = 0  #atmospheric water deficit for now
     
     P = alpha * G                       #calculate photosynthesis and carbon allocation to:
     R = beta * S * (1 - K/kmax)         #respiration from xylem repair
-    B = gamma * s * exp(-(-X/l.B)^k.B)  #biomass growth
+    B = gamma * S * exp(-(-X/l.B)^k.B)  #biomass growth
     M = m                               #maintenance respiration
     
     dS = P - R - M - B  #daily change in TNC, g d-1
@@ -56,4 +57,7 @@ tree_odes = function(time, states, parms) {
   })
 }
 
-lsoda(states0, times, tree_odes, parms)
+require(deSolve)
+modelout = lsoda(states0, times, tree_odes, parms)
+par(mfrow=c(3,3))
+plot(modelout[,1], modelout[,4], type="l")
