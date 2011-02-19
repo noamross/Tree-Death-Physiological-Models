ee3c53c14ea330db11a5a07306f17c3649664c34
noamross <noam.ross@gmail.com>
2011-02-18 16:20:57 -0800

    Parameter Testing

--- R/treefunctions.R
+++ R/treefunctions.R
@@ -1,11 +1,10 @@
 #Functions and Code for Tree Death Physiology Model
 #Noam Ross Started February 15, 2011
-#Test web edit
 
 #Set run variables
-times = 1:120 #times at which to solve the model
+times = 1:500 #times at which to solve the model
 states0 = c( #a vector of initial state variables
-  W = -1, #soil water potential, MPa
+  W = -1.5, #soil water potential, MPa
   D = -2, #leaf surface moisture saturation deficit, MPa
   S = 5, #total non-structural carbohydrates, g
   K = 1 #xylem conductance, mmol s-1 MPa-1 
@@ -13,21 +12,21 @@ states0 = c( #a vector of initial state variables
 
 
 parms = list(
-  alpha = 1, #photsynthetic rate per stomatal conductane, in g s MPa mmol-1 d-1
-  beta = 1, #repair coefficient in g g-1 d-1
-  gamma = 1, #growth coefficient in g g-1 d-1
-  theta = 1, #repair efficacy, in mmol s-1 MPa-1 d-1 g-1
-  kmax = 10, #maximum hydraulic contuctance, in mmol s-1 MPa-1
-  m = 0, #minimum maintenance respiration carb allocation, in g d-1
+  alpha = 1, #photsynthetic rate per stomatal conductance, in g s MPa mmol-1 d-1
+  beta = .01, #repair coefficient in g g-1 d-1
+  gamma = 0, #growth coefficient in g g-1 d-1
+  theta = 0.1, #repair efficacy, in mmol s-1 MPa-1 d-1 g-1
+  kmax = 1, #maximum hydraulic contuctance, in mmol s-1 MPa-1
+  m = 0.1, #minimum maintenance respiration carb allocation, in g d-1
   l.B = 1, #scale parameter for growth weibull function
   k.B = 1, #shape parameter for growth weibull function
-  l.K = 1, #scale parameter for conductance weibull function
-  k.K = 1, #shape paramter for conductance weibull function
+  l.K = 8, #scale parameter for conductance weibull function, from Pinyon branches in Linton eet al. 1998
+  k.K = 2, #shape paramter for conductance weibull function, from Pinyon branches in Linton et al. 1998 
   #Gmin = 0 , #minimum stomatal conductance, mmol s-1 MPa-1
   gmax = 1,  #maximum stomatal conductance, mmol s-1 MPa-1
   #l.G = 1 , #scale parameter for stomatal closing weibull function
   #k.G = 5 , #shape parameter for stomatal closing weibull function
-  gs = 1, #stomatal closure factor
+  gs = 0.5, #stomatal closure factor
   wpg = 0.5 #water potential of gravity, MPa
 )
   
@@ -41,7 +40,9 @@ tree_odes = function(time, states, parms) {
   
   with(c(as.list(states), parms), { #extract parameters from 'parms' vector
     X = (W - D*gmax/K - wpg)/(1 + D/(K*gs)) # Calculate xylem pressure
-    G = gmax - (-X/gs) # Calculate stomatal flow.  
+    #G = gmax - (-X/gs) # Calculate stomatal flow.  
+    G = gmax - (-X/gs) # Calculate stomatal flow
+    
     
     dW = 0  #no change in soil water potential or 
     dD = 0  #atmospheric water deficit for now
@@ -49,16 +50,48 @@ tree_odes = function(time, states, parms) {
     P = alpha * G                       #calculate photosynthesis and carbon allocation to:
     R = beta * S * (1 - K/kmax)         #respiration from xylem repair
     B = gamma * S * exp(-(-X/l.B)^k.B)  #biomass growth
-    M = m                               #maintenance respiration
+    M = m*S                              #maintenance respiration
     
     dS = P - R - M - B  #daily change in TNC, g d-1
-    dK = -(k.K/l.K)*((-X/l.K)^(k.K-1))*exp(-(-X/l.K)^k.K) + theta*R #daily change in xylem conductance, mmol s-1 MPa-1 d-1 
+   # dK = -K*(k.K/l.K)*((-X/l.K)^(k.K-1))*exp(-((-X/l.K)^k.K)) + theta*R #daily change in xylem conductance, mmol s-1 MPa-1 d-1 
+    dK = -K*(k.K/l.K)*((-X/l.K)^(k.K-1))*exp(-((-X/l.K)^k.K)) + theta*R #daily change in xylem conductance, mmol s-1 MPa-1 d-1 
     
-    return(list(c(dW,dD,dS,dK),c(X=X, G=G, P=P, R=R, B=B, M=M))) 
+    return(list(c(dW,dD,dS,dK),c(X=X, G=G, P=P, R=R, B=B, M=M)))   #return a list of of both derivatives and other state variables
   })
 }
 
 require(deSolve)
 modelout = lsoda(states0, times, tree_odes, parms)
-par(mfrow=c(3,3))
-plot(modelout[,1], modelout[,4], type="l")
+save(times, states0, parms, modelout,     #save parameters and output data in ASCII file named "Modelrun_YYYYMMDD_HHMM.R"
+     file=paste("Outputs/Modelrun_" ,format(Sys.time(), "%Y%m%d_%H%M"), ".R", sep=""), 
+     ascii=TRUE)
+
+pdf(paste(file="Outputs/Modelrun_", format(Sys.time(), "%Y%m%d_%H%M%S"), ".pdf", sep=""))#create a pdf file to store outputs
+  par(mfrow=c(2,4)) #make a 2X4 grid of graphs
+  plot(times, modelout[,"W"], type="l") #plot output variables
+  plot(times, modelout[,"D"], type="l")
+  plot(times, modelout[,"X"], type="l")
+  plot(times, modelout[,"G"], type="l")
+  plot(times, modelout[,"K"], type="l")
+  plot(times, modelout[,"P"], type="l")  #plot all the photsynthsis variables on one graph
+  lines(times, modelout[,"R"], type="l")
+  lines(times, modelout[,"B"], type="l")
+  lines(times, modelout[,"M"], type="l")
+  plot(times, modelout[,"S"], type="l")
+dev.off() #close the pdf
+
+par(mfrow=c(2,4)) #make a 2X4 grid of graphs
+plot(times, modelout[,"W"], type="l")
+plot(times, modelout[,"D"], type="l")
+plot(times, modelout[,"X"], type="l")
+plot(times, modelout[,"G"], type="l")
+plot(times, modelout[,"K"], type="l")
+plot(times, modelout[,"P"], type="l", col="green")
+lines(times, modelout[,"R"], type="l", col="red")
+lines(times, modelout[,"B"], type="l", col="black")
+lines(times, modelout[,"M"], type="l", col="blue")
+plot(times, modelout[,"S"], type="l")
+
+#par(mfrow=c(1,1))  #set up a single graph
+plot(modelout[,"X"], modelout[,"K"], type="l")  #plot xylem pressure against conductance
+
